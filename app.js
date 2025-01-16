32591f5caca5a69f3000a9673fd7f14c4fef87a2
caue <caueabproenca@gmail.com>
2025-01-16 10:55:09 -0300

    Added logic for handling request params

--- app.js
+++ app.js
@@ -19,12 +19,31 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+
+  if (tours.find((el) => el.id === id)) {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: tour,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: 'Failure',
+      message: 'Invalid ID',
+    });
+  }
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
-  
+
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
@@ -32,7 +51,7 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({
         satus: 'success',
         data: {
-          tour: newTour
+          tour: newTour,
         },
       });
     }

fbf38947605e7d30c9b7547f1ef4d3bdfe8475d5
caue <caueabproenca@gmail.com>
2025-01-16 08:55:11 -0300

    Set up basic routing

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+    res.status(200).send('You can post it to this endpoint');
+})
 
 const port = 3000;
 app.listen(port, () => {

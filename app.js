bd2d70a8608e1714c3e96b54434a3861fe17c2e8
caue <caueabproenca@gmail.com>
2025-01-16 18:54:51 -0300

    Added toy deleting logic

--- app.js
+++ app.js
@@ -74,6 +74,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   }
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (parseInt(req.params.id) > tours.length) {
+    res.status(404).json({
+      status: 'Failure',
+      message: 'Invalid ID',
+    });
+  } else {
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  }
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);

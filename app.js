21ff65dfa5b275f37972fe55ef266d21f7497f8a
caue <caueabproenca@gmail.com>
2025-01-17 20:43:44 -0300

    Set up environment variables

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const userRouter = require('./routes/user-routes');
 const app = express();
 
 // MIDDLEWARES
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
@@ -14,7 +17,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
 
 // ROUTERS MOUNT
 app.use('/api/v1/tours', tourRouter);

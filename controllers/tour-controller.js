c5c749c6dec34cd7436d883b90b6f41131e0ddb3
caue <caueabproenca@gmail.com>
2025-01-22 16:45:53 -0300

    Added simple query related logic to getAllTours method

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -2,11 +2,20 @@ const Tour = require('../models/tour-model');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       message: 'success',
-      results: tours.lentgh,
+      results: tours.length,
       data: {
         tours: tours,
       },

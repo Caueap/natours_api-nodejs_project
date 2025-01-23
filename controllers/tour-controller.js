9c49d2eb92d4dc3dc480ca5d32f88e401a99392a
caue <caueabproenca@gmail.com>
2025-01-23 17:39:57 -0300

    Implemented pagination for getAllTours method

--- controllers/tour-controller.js
+++ controllers/tour-controller.js
@@ -30,6 +30,23 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // BUILD QUERY - PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exist');
+      }
+    }
+
+    // How the query is looking like:
+    // query.find().sort().select().skip().limit()
+
     // EXECUTE QUERY
     const tours = await query;
 

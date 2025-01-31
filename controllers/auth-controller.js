0cd3a9b5ddc8c57e663a1289a2957c37fe925d0c
caue <caueabproenca@gmail.com>
2025-01-31 19:35:31 -0300

    Refactored response sent to client on signup, implemented creation of jwt on singup, added logic for checking if password and confirmPassword are equal, added middleware to encrypt password

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,11 +1,23 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/user-model');
 const catchAsync = require('../utils/catch-async');
+// const AppError = require('../utils/app-error');
 
 exports.signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },

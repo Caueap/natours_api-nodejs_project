4e9bec0785cd7dfc919b5ebf777e0f580c6625f9
caue <caueabproenca@gmail.com>
2025-02-06 19:52:27 -0300

    Implemented data sanitazation for protection against nosql injection; Implemented xss protection

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/app-error');
 const globalErrorHandler = require('./controllers/error-controller');
@@ -29,6 +31,12 @@ app.use('/api', limiter);
 
 // Body parser, reads data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 app.use(express.static(`${__dirname}/public`));
 

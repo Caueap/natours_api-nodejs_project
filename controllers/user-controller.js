30f8c6fd114d98477640df3bbb3ef665a90f0f22
caue <caueabproenca@gmail.com>
2025-02-06 16:59:13 -0300

    Implemented functionality for updating an user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,14 @@
 const User = require('../models/user-model');
 const catchAsync = require('../utils/catch-async');
+const AppError = require('../utils/app-error');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -9,6 +18,37 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     results: users.length,
     data: {
       users: users,
+    },
+  });
+});
+
+exports.updateCurrentUser = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm)
+    return next(new AppError('This route is not for password updates.', 400));
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // if (
+  //   Object.keys(filteredBody).forEach((el) => {
+  //     if (el !== 'name' || el !== 'email')
+  //       return next(
+  //         new AppError(
+  //           'Only name and email can be updated on this endpoint',
+  //           400,
+  //         ),
+  //       );
+  //   })
+  // );
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedUser,
     },
   });
 });

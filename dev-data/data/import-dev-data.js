0f2ed1c34608944d73957ec9536f92d6d4e0a95a
caue <caueabproenca@gmail.com>
2025-02-16 21:40:04 -0300

    Added start location, locations and guides to the tour model; Implemented child referencing of guides in the tour model and populated the guides field using query middleware

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -16,9 +16,7 @@ mongoose.connect(DB, {
   useFindAndModify: false,
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 const importData = async () => {
   try {

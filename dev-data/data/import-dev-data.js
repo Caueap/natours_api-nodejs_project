2f5153554a4a8defe729e764c84dcd6a28826932
caue <caueabproenca@gmail.com>
2025-02-19 05:47:54 -0300

    imported new data to the database

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tour-model');
+const User = require('../../models/user-model');
+const Review = require('../../models/review-model');
 
 dotenv.config({ path: './config.env' });
 
@@ -17,10 +19,16 @@ mongoose.connect(DB, {
 });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded');
   } catch (e) {
     console.log(e);
@@ -31,6 +39,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted');
   } catch (e) {
     console.log(e);

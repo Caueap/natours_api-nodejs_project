aa1bb5779dd86146b7931e069c0cd3888d0a764a
caue <caueabproenca@gmail.com>
2025-02-19 05:23:59 -0300

    Refactored routes authentications and authorizations

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -10,30 +10,38 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+// As middleware runs in sequence, all code below will
+// use the authController.protect. So, there's no need
+// to add authController.protect to each router below
+router.use(authController.protect);
+
 router.get(
   '/getCurrentUser',
-  authController.protect,
+  // authController.protect,
   userController.getCurrentUser,
   userController.getUser,
 );
 
 router.patch(
   '/updateCurrentUser',
-  authController.protect,
+  // authController.protect,
   userController.updateCurrentUser,
 );
 
 router.patch(
   '/updatePassword',
-  authController.protect,
+  // authController.protect,
   authController.updatePassword,
 );
 
 router.delete(
   '/deleteCurrentUser',
-  authController.protect,
+  // authController.protect,
   userController.deleteCurrentUser,
 );
+
+// Applying the same principle of line 13
+router.use(authController.restrictTo('admin'));
 
 router.route('/').get(userController.getAllUsers);
 
